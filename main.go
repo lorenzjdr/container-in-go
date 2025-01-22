7505f2e96bde3272f5abdd46e6b4f867c0814cb7
lorenzjdr <lorenz.jdr@gmail.com>
2025-01-21 21:18:08 -0600

    Added comments for my understanding

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os/exec"
 )
 
+// Checks for user arguements
 func main() {
 	switch os.Args[1] {
 	case "run":
@@ -17,19 +18,26 @@ func main() {
 	}
 }
 
+// Re-run the current program as a child
 func parent() {
+	// /proc/self/exe is a symlink to the current program
 	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+
+	// Set the command's standard input, output, and error to the current process's standard input, output, and error
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
+	// Run the command and check for errors
 	if err := cmd.Run(); err != nil {
 		fmt.Println("ERROR", err)
 		os.Exit(1)
 	}
 }
 
-func child() { 
+// Run the user's program 
+func child() {
+	
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
@@ -40,7 +48,7 @@ func child() {
 		os.Exit(1)
 	}
 }
-
+// Error handling
 func must(err error) {
 	if err != nil {
 		panic(err)
